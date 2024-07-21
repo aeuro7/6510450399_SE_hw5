3572277c5836d39c0c2a86bdd425c08f0c56d316
euro <tibet.c@ku.th>
2024-07-21 23:00:45 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+
+        System.out.println("Test 1");
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+        System.out.println("-----------");
+
+        System.out.println("Test 2");
+        gumballMachine.insertQuarter();
+        gumballMachine.chooseFlavor("Mango");
+        gumballMachine.chooseFlavor("Orange");
+        gumballMachine.turnCrank();
+        System.out.println(gumballMachine);
+
+    }
+}
